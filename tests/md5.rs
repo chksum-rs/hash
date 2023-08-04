da870126449244bf4c771343880b5fafd16c8844
Konrad Goławski <konrad@golawski.it>
2023-08-04 23:39:43 +0200

    Fixed tests code format to be rustfmt compatible

--- tests/md5.rs
+++ tests/md5.rs
@@ -3,38 +3,17 @@ use chksum_hash::md5;
 #[test]
 fn test_empty() {
     let digest = md5::hash("");
-    #[rustfmt::skip]
-    assert_eq!(
-        digest.to_hex_lowercase(),
-        "d41d8cd98f00b204e9800998ecf8427e"
-    );
+    assert_eq!(digest.to_hex_lowercase(), "d41d8cd98f00b204e9800998ecf8427e");
 
     let digest = md5::new().digest();
-    #[rustfmt::skip]
-    assert_eq!(
-        digest.to_hex_lowercase(),
-        "d41d8cd98f00b204e9800998ecf8427e"
-    );
+    assert_eq!(digest.to_hex_lowercase(), "d41d8cd98f00b204e9800998ecf8427e");
 }
 
 #[test]
 fn test_hello_world() {
     let digest = md5::hash("Hello World!");
-    #[rustfmt::skip]
-    assert_eq!(
-        digest.to_hex_lowercase(),
-        "ed076287532e86365e841e92bfc50d8c"
-    );
+    assert_eq!(digest.to_hex_lowercase(), "ed076287532e86365e841e92bfc50d8c");
 
-    #[rustfmt::skip]
-    let digest = md5::new()
-        .update("Hello")
-        .update(" ")
-        .update("World!")
-        .digest();
-    #[rustfmt::skip]
-    assert_eq!(
-        digest.to_hex_lowercase(),
-        "ed076287532e86365e841e92bfc50d8c"
-    );
+    let digest = md5::new().update("Hello").update(" ").update("World!").digest();
+    assert_eq!(digest.to_hex_lowercase(), "ed076287532e86365e841e92bfc50d8c");
 }
--- tests/sha1.rs
+++ tests/sha1.rs
@@ -3,38 +3,17 @@ use chksum_hash::sha1;
 #[test]
 fn test_empty() {
     let digest = sha1::hash("");
-    #[rustfmt::skip]
-    assert_eq!(
-        digest.to_hex_lowercase(),
-        "da39a3ee5e6b4b0d3255bfef95601890afd80709"
-    );
+    assert_eq!(digest.to_hex_lowercase(), "da39a3ee5e6b4b0d3255bfef95601890afd80709");
 
     let digest = sha1::new().digest();
-    #[rustfmt::skip]
-    assert_eq!(
-        digest.to_hex_lowercase(),
-        "da39a3ee5e6b4b0d3255bfef95601890afd80709"
-    );
+    assert_eq!(digest.to_hex_lowercase(), "da39a3ee5e6b4b0d3255bfef95601890afd80709");
 }
 
 #[test]
 fn test_hello_world() {
     let digest = sha1::new().update("Hello World!").finalize().digest();
-    #[rustfmt::skip]
-    assert_eq!(
-        digest.to_hex_lowercase(),
-        "2ef7bde608ce5404e97d5f042f95f89f1c232871"
-    );
+    assert_eq!(digest.to_hex_lowercase(), "2ef7bde608ce5404e97d5f042f95f89f1c232871");
 
-    #[rustfmt::skip]
-    let digest = sha1::new()
-        .update("Hello")
-        .update(" ")
-        .update("World!")
-        .digest();
-    #[rustfmt::skip]
-    assert_eq!(
-        digest.to_hex_lowercase(),
-        "2ef7bde608ce5404e97d5f042f95f89f1c232871"
-    );
+    let digest = sha1::new().update("Hello").update(" ").update("World!").digest();
+    assert_eq!(digest.to_hex_lowercase(), "2ef7bde608ce5404e97d5f042f95f89f1c232871");
 }
